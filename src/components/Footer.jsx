ffc9ae632d560fd0bd992dff9bcad1df3199bef3
shishir bhattarai <shishirbhattarai2002@gmail.com>
2026-06-13 21:48:09 +0545

    site edited  fonts adjusted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,7 +48,7 @@ export default function Footer() {
             <div className={styles.social}>
               <SocialIcon
                 title="Facebook"
-                href="https://www.facebook.com/profile.php?id=615858241817500"
+                href="https://www.facebook.com/profile.php?id=61585824181750"
                 path={<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />}
               />
               <SocialIcon
